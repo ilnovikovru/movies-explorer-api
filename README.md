78e735a7f56ef25a60c9540426451be29b64bc81
Ilya Novikov <ilnovikov@ilnovikov.ru>
2023-07-30 13:16:10 +0100

    Initial commit

--- README.md
+++
@@ -1,3 +0,0 @@
-IP 51.250.102.17
-
-Backend https://ilnovikov-movies.nomoredomains.sbs/api/
--- errors/ForbiddenError.js
+++
@@ -1,9 +0,0 @@
-class ForbiddenError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 403;
-    this.name = 'ForbiddenError';
-  }
-}
-
-module.exports = ForbiddenError;
